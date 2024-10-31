ece4d211bed9ebe174663cafa3d82954b9fa0924
AlinaKalashnyk <alina.kalashnyk@ringteam.com>
2024-10-31 18:46:01 +0200

    HOMEWORK 8

--- src/SumAverage.java
+++ src/SumAverage.java
@@ -1,51 +1,45 @@
-/**
- * Compute the sum and average for running integers using loop.
- */
 public class SumAverage {
 
-    /**
-     * Computes sum of running integers from a lower bound to an upper bound.
-     *
-     * @param lowerBound lower bound
-     * @param upperBound upper bound
-     * @return sum of integers
-     */
     public static int sum(int lowerBound, int upperBound) {
-        // TODO fill in code here using for loop and replace the return statement
-        return -1;
+        int sum = 0;
+        for (int i = lowerBound; i <= upperBound; i++) {
+            sum += i;
+        }
+        return sum;
     }
 
-    /**
-     * Computes sum of running integers from a lower bound to an upper bound within an array.
-     *
-     * @param numbers running integers array
-     * @return sum of integers
-     */
     public static int sum(int[] numbers) {
-        // TODO fill in code here using for each loop and replace the return statement
-        return -1;
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
     }
 
-    /**
-     * Computes average of running integers from a lower bound to an upper bound.
-     *
-     * @param lowerBound lower bound
-     * @param upperBound upper bound
-     * @return average of integers
-     */
     public static double average(int lowerBound, int upperBound) {
-        // TODO fill in code here using while loop and replace the return statement
-        return -1;
+        int sum = 0;
+        int count = 0;
+        int i = lowerBound;
+
+        while (i <= upperBound) {
+            sum += i;
+            i++;
+            count++;
+        }
+        return (double) sum / count;
     }
 
-    /**
-     * Computes average of running integers from a lower bound to an upper bound within an array.
-     *
-     * @param numbers running integers array
-     * @return average of integers
-     */
     public static double average(int[] numbers) {
-        // TODO fill in code here using do-while loop and replace the return statement
-        return -1;
+        int sum = 0;
+        int count = 0;
+        int i = 0;
+
+        do {
+            sum += numbers[i];
+            count++;
+            i++;
+        } while (i < numbers.length);
+
+        return (double) sum / count;
     }
 }
--- src/SumAverageTest.java
+++ src/SumAverageTest.java
@@ -1,39 +1,31 @@
-import static org.testng.Assert.assertEquals;
-
-import java.util.stream.IntStream;
-
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.stream.IntStream;
+
+import static org.testng.Assert.assertEquals;
+
 public class SumAverageTest {
 
-    /**
-     * Test data for lower and upper bounds.
-     *
-     * @return test data array
-     */
     @DataProvider(name = "Sum")
     public static Object[][] sum() {
         return new Object[][]{
                 {1, 100, 5050},
                 {-100, 100, 0},
-                {1, 1, 1}
-                // TODO add 2 more test data here
+                {1, 1, 1},
+                {50, 60, 605},
+                {-50, 50, 0}
         };
     }
 
-    /**
-     * Test data for lower and upper bounds.
-     *
-     * @return test data array
-     */
     @DataProvider(name = "Average")
     public static Object[][] average() {
         return new Object[][]{
                 {1, 100, 50.5},
                 {-100, 100, 0.0},
-                {1, 1, 1.0}
-                // TODO add 2 more test data here
+                {1, 1, 1.0},
+                {50, 60, 55.0},
+                {-50, 50, 0.0}
         };
     }
 
