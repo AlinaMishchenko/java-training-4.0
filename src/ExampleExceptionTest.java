6aa6e71abaa400c6ce31eb7a9d091d5be02bde55
Alina Kalashnyk <alina.kalashnyk@squad.tech>
2024-11-08 14:27:02 +0200

    HOMEWORK 10

--- src/ExampleExceptionTest.java
+++ src/ExampleExceptionTest.java
@@ -1,36 +1,25 @@
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.fail;
-
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class ExampleExceptionTest {
 
-    /**
-     * Test data for positive test.
-     *
-     * @return test data array
-     */
     @DataProvider(name = "data")
     public static Object[][] data() {
         return new Object[][]{
                 {2, 2, 4},
-                {2, 3, 6}
-                // TODO add 2 more test data here
+                {2, 3, 6},
+                {3, 5, 8},
+                {4, 4, 6}
         };
     }
 
-    /**
-     * Test data for exception test.
-     *
-     * @return test data array
-     */
     @DataProvider(name = "negativeData")
     public static Object[][] negativeData() {
         return new Object[][]{
                 {-2, 2},
-                {2, -2}
-                // TODO add 2 more test data here
+                {2, -2},
+                {4, -4},
+                {-4, 4}
         };
     }
 
