23e5750e61949b5b65d415ad331d061bf1afcad1
Alina Kalashnyk <alina.kalashnyk@squad.tech>
2024-11-14 18:32:44 +0200

    HOMEWORK 10

--- src/StreamTaskTest.java
+++ src/StreamTaskTest.java
@@ -19,8 +19,12 @@ public class StreamTaskTest {
                 {
                         new int[]{13, -2, 33, -4, 5},
                         20
-                }
-                // TODO add 2 more test data here
+                },
+                {
+                        new int[]{10, 0, 8, 3}, 164
+                },
+                {
+                        new int[]{2, 4, 6, 8, 10}, 220}
         };
     }
 
@@ -38,8 +42,16 @@ public class StreamTaskTest {
                 {
                         new int[]{130, -200, 330, -40, 50, 66},
                         new int[]{}
+                },
+                {
+                        new int[]{9, 3, 7, 11},
+                        new int[]{3, 7, 9, 11}
+                },
+                {
+                        new int[]{0, -1, -3, 4, 3},
+                        new int[]{-3, -1, 3}
                 }
-                // TODO add 2 more test data here
+
         };
     }
 
